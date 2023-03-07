9edc47cc803f5312bac6a595d5483e660a3ded45
Jerrssey <fankton@gmail.com>
2023-03-07 14:32:29 +0200

    task 02/2 step 1,2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const name = evt.currentTarget.elements.name.value;
+    const number = evt.currentTarget.elements.number.value;
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+    evt.currentTarget.elements.name.value = this.state.name;
+    evt.currentTarget.elements.number.value = this.state.number;
+    evt.reset();
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h2>Phonebook</h2>
+          <form onSubmit={this.handleSubmit} autoComplete="off">
+            <label htmlFor="">
+              Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+            <label htmlFor="">
+              Phone
+              <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            {this.state.contacts.map(contact => {
+              return (
+                <li key={contact.id}>
+                  {contact.name}: {contact.number}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
